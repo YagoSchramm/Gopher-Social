5db8c68be717497ac236aa95b8c5ab491ab839d0
YagoSchramm <yrtoiu1515@gmail.com>
2026-04-29 21:58:05 -0300

    chore(): Created the datastore interface

--- app/internal/infrastructure/datastore.go
+++ app/internal/infrastructure/datastore.go
@@ -15,43 +15,43 @@ var (
 	QueryTimeoutDuration = time.Second * 5
 )
 
-type Storage struct {
-	Posts interface {
-		GetByID(context.Context, int64) (*domain.Post, error)
-		Create(context.Context, *domain.Post) error
-		Delete(context.Context, int64) error
-		Update(context.Context, *domain.Post) error
-		GetUserFeed(context.Context, int64, domain.PaginatedFeedQuery) ([]domain.PostWithMetadata, error)
-	}
-	Users interface {
-		GetByID(context.Context, int64) (*domain.User, error)
-		GetByEmail(context.Context, string) (*domain.User, error)
-		Create(context.Context, *sql.Tx, *domain.User) error
-		CreateAndInvite(ctx context.Context, user *domain.User, token string, exp time.Duration) error
-		Activate(context.Context, string) error
-		Delete(context.Context, int64) error
-	}
-	Comments interface {
-		Create(context.Context, *domain.Comment) error
-		GetByPostID(context.Context, int64) ([]domain.Comment, error)
-	}
-	Followers interface {
-		Follow(ctx context.Context, userID, followerID int64) error
-		Unfollow(ctx context.Context, followerID, userID int64) error
-	}
-	Roles interface {
-		GetByName(context.Context, string) (*domain.Role, error)
-	}
+type Datastore interface {
+	Posts() PostRepository
+	Users() UserRepository
+	Comments() CommentRepository
+	Followers() FollowerRepository
+	Roles() RoleRepository
 }
 
-func NewStorage(db *sql.DB) Storage {
-	return Storage{
-		Posts:     &PostStore{db},
-		Users:     &UserStore{db},
-		Comments:  &CommentStore{db},
-		Followers: &FollowerStore{db},
-		Roles:     &RoleStore{db},
-	}
+type PostRepository interface {
+	GetByID(context.Context, int64) (*domain.Post, error)
+	Create(context.Context, *domain.Post) error
+	Delete(context.Context, int64) error
+	Update(context.Context, *domain.Post) error
+	GetUserFeed(context.Context, int64, domain.PaginatedFeedQuery) ([]domain.PostWithMetadata, error)
+}
+
+type UserRepository interface {
+	GetByID(context.Context, int64) (*domain.User, error)
+	GetByEmail(context.Context, string) (*domain.User, error)
+	Create(context.Context, *sql.Tx, *domain.User) error
+	CreateAndInvite(ctx context.Context, user *domain.User, token string, exp time.Duration) error
+	Activate(context.Context, string) error
+	Delete(context.Context, int64) error
+}
+
+type CommentRepository interface {
+	Create(context.Context, *domain.Comment) error
+	GetByPostID(context.Context, int64) ([]domain.Comment, error)
+}
+
+type FollowerRepository interface {
+	Follow(ctx context.Context, userID, followerID int64) error
+	Unfollow(ctx context.Context, followerID, userID int64) error
+}
+
+type RoleRepository interface {
+	GetByName(context.Context, string) (*domain.Role, error)
 }
 
 func withTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
